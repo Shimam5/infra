d663f1dd26b94b9c568268dbea366630cab1694e
S Himam <111451625+Shimam5@users.noreply.github.com>
2022-12-08 00:45:46 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
 # Creating VPC
-resource "aws_vpc" "demovpc" {
-cidr_block = "90.9.0.0/16 "
+resource "aws_vpc" "newdemovpc" {
+cidr_block = "40.0.0.0/16"
 instance_tenancy = "default"
 tags = {
-Name = "Demo VPC"
+Name = "Terraform VPC"
 }
 }
