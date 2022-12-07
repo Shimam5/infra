7b84534b22b084b03e0bf8b97cf288e87f6227bc
S Himam <111451625+Shimam5@users.noreply.github.com>
2022-12-08 01:12:49 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,6 @@
 # Creating 1st web subnet
 resource "aws_subnet" "public-subnet-1" {
-vpc_id = "${aws_vpc.demovpc.id}"
+vpc_id = "${aws_vpc.newdemovpc.id}"
 cidr_block = "${var.subnet_cidr}"
 map_public_ip_on_launch = true
 availability_zone = "us-east-1a"
@@ -10,9 +10,9 @@ Name = "Web Subnet 1"
 }
 # Creating 2nd web subnet
 resource "aws_subnet" "private-subnet-2" {
-vpc_id = "${aws_vpc.demovpc.id}"
+vpc_id = "${aws_vpc.newdemovpc.id}"
 cidr_block = "${var.subnet1_cidr}"
-map_public_ip_on_launch = true
+map_public_ip_on_launch = false
 availability_zone = "us-east-1b"
 tags = {
 Name = "Web Subnet 2"
